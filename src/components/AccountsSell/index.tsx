e8dcd42a4a9fdfaafed52db39e2a6a02bc9e6084
Christopher Yovanovitch <christopher.yovanovitch@link-value.fr>
2019-04-29 16:40:47 +0200

    wip

--- src/components/AccountsSell/index.tsx
+++ src/components/AccountsSell/index.tsx
@@ -1,13 +1,22 @@
 import React, { FC, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { useTranslation } from 'react-i18next';
-import { Grid, Link, Typography } from '@material-ui/core';
+import { Grid, Link, Typography, Card, CardContent, CardActions, Button } from '@material-ui/core';
 import { useOvermind } from '../../overmind';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1
   },
+  card: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  }
 });
 
 const AccountsSell: FC = () => {
@@ -26,15 +35,43 @@ const AccountsSell: FC = () => {
   return (
     <div className={classes.root}>
       <Grid container>
-        <Typography variant="h3">{t("accountsSell")} ({state.github.accounts.count})</Typography>
+        <Typography variant="h3">{t("accountsSellDetails", { count: state.github.accounts.count })}</Typography>
+        <br/><br/>
         {state.github.accounts.count >= 10 && <Grid item md={6}>
-          <Link onClick={refreshAccounts} href="https://commerce.coinbase.com/checkout/68a462d6-04c7-4f06-b317-390ec270e410">10</Link>
+          <Card className={classes.card}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {t('account', { count: 10 })}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button href="https://commerce.coinbase.com/checkout/68a462d6-04c7-4f06-b317-390ec270e410" size="small">{t('buy')}</Button>
+          </CardActions>
+        </Card>
         </Grid>}
         {state.github.accounts.count >= 50 && <Grid item md={6}>
-          <Link onClick={refreshAccounts} href="https://commerce.coinbase.com/checkout/c24cd51b-1c2d-405d-9218-fca2648c7c9a">50</Link>
+          <Card className={classes.card}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {t('account', { count: 50 })}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button href="https://commerce.coinbase.com/checkout/c24cd51b-1c2d-405d-9218-fca2648c7c9a" size="small">{t('buy')}</Button>
+          </CardActions>
+        </Card>
         </Grid>}
         {state.github.accounts.count >= 100 && <Grid item md={6}>
-          <Link onClick={refreshAccounts} href="https://commerce.coinbase.com/checkout/758baa93-29b8-42f7-833a-43c6881f191f">100</Link>
+          <Card className={classes.card}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {t('account', { count: 100 })}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button href="https://commerce.coinbase.com/checkout/758baa93-29b8-42f7-833a-43c6881f191f" size="small">{t('buy')}</Button>
+          </CardActions>
+        </Card>
         </Grid>}
       </Grid>
     </div>
